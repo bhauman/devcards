a68f7f5fc1676936ec28985e68fbac892e0aac33
Bruce Hauman <bhauman@gmail.com>
2014-12-20 10:43:04 -0500

    cleaning up project.clj a bit

--- project.clj
+++ project.clj
@@ -14,24 +14,22 @@
                  [frontier "0.1.0-SNAPSHOT"]
                  [figwheel "0.2.0-SNAPSHOT"]]
 
-  :aliases {"clean" ["do" "cljsbuild" "clean," "clean"]}
-
-  :profiles {
-             :dev {
-                   :plugins [[lein-cljsbuild "1.0.3"]
-                             [lein-figwheel "0.2.0-SNAPSHOT"]]
-                   }
-             }
-
   :source-paths ["src"]
-
-  :cljsbuild {
-    :builds [{:id "devcards-demos"
-              :source-paths ["src" "example_src"]
-              :compiler {
-                         :output-to "resources/public/devcards/js/compiled/devdemos.js"
-                         :output-dir "resources/public/devcards/js/compiled/out"
-                         :optimizations :none
-                         :source-map true}}]}
   
-  :figwheel { :css-dirs ["resources/public/devcards/css"] })
+  :profiles {
+    :dev {
+      :aliases {"clean" ["do" "cljsbuild" "clean," "clean"]}
+                   
+      :plugins [[lein-cljsbuild "1.0.3"]
+                [lein-figwheel "0.2.0-SNAPSHOT"]]
+      :cljsbuild {
+        :builds [{:id "devcards-demos"
+                  :source-paths ["src" "example_src"]
+                  :compiler {
+                    :output-to "resources/public/devcards/js/compiled/devdemos.js"
+                    :output-dir "resources/public/devcards/js/compiled/out"
+                    :optimizations :none
+                    :source-map true}}]}
+                   
+      :figwheel { :css-dirs ["resources/public/devcards/css"] }
+     }})
