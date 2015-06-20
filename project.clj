9c6f4d041068251d03b7dac788ec84a59c69152d
Bruce Hauman <bhauman@gmail.com>
2015-06-20 15:10:32 -0400

    bump to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject devcards "0.1.4-SNAPSHOT"
+(defproject devcards "0.2.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
