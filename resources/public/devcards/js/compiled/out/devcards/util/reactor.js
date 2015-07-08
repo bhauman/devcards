4af6c52956aad57f2bcce82284975e93e25461cd
Bruce Hauman <bhauman@gmail.com>
2015-07-08 16:18:07 -0400

    removing resources

--- resources/public/devcards/js/compiled/out/devcards/util/reactor.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 0.0-2202
-goog.provide('devcards.util.reactor');
-goog.require('cljs.core');
-devcards.util.reactor.Pure = React.createClass((function (){var obj11650 = {"shouldComponentUpdate":(function (next_props,next_state){var this$ = this;return !(cljs.core._EQ_.call(null,this$.props.value,next_props.value));
-}),"render":(function (){var this$ = this;return this$.props.children.call(null);
-})};return obj11650;
-})());
-
-//# sourceMappingURL=reactor.js.map
--- resources/public/devcards/js/compiled/out/devdemos/blank.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 0.0-2202
-goog.provide('devdemos.blank');
-goog.require('cljs.core');
-devdemos.blank.hello = (function hello(){return "hellorrr";
-});
-
-//# sourceMappingURL=blank.js.map
