6d25cb84b73de5133e38b024335491d139b52c39
Alex Gunnarson <alexandergunnarson@gmail.com>
2016-02-08 00:40:17 -0700

    Added DataScript rendering support
    - Added two lines of code in core.cljs in order to add the :render-fn
    parameter to e.g. `defcard-rg`
    - Added one function to util/utils.cljs — `ds->seq` — in order to
    correctly render DataScript databases via Devcards. Provided
    documentation for this

--- src/devcards/util/utils.cljs
+++ src/devcards/util/utils.cljs
@@ -21,3 +21,25 @@
  
  (defn pprint-code [code]
    (pprint/with-pprint-dispatch pprint/code-dispatch (pprint-str code)))
+
+(defn ds->seq
+  "Renders a DataScript database in a way that Devcards can handle.
+   
+   Use this as the :render-fn parameter to `defcard-rg` when the devcards
+   data atom is a DataScript database.
+
+   Usage:
+   (defcard-rg my-card
+     \"Doesn't do much.\"
+     [:div]
+     datascript-conn
+     {:inspect-data true
+      :history      true
+      :render-fn    ds->seq})"
+  [db]
+  (->> db
+       (map (fn [d] {:e     (:e     d)
+                     :a     (:a     d)
+                     :v     (:v     d)
+                     :tx    (:tx    d)
+                     :added (:added d)}))))
