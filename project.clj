ae6d1a459b91cfbccb4594c0e6970b2de289a4b3
Bruce Hauman <bhauman@gmail.com>
2017-11-13 09:12:39 -0500

    bump to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject devcards "0.2.4"
+(defproject devcards "0.2.5-SNAPSHOT"
   :description "Devcards is a ClojureScript library that provides a lab space in which you can develop your UI components independently and interactively."
   :url "http://github.com/bhauman/devcards"
   :license {:name "Eclipse Public License"
